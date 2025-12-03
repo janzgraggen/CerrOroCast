c912a995146ea1de3007be84f816a11677fef770
janzgraggen <jan.zgraggen@epfl.com>
2025-12-03 11:30:30 +0100

    add the if __main__ logic for better  modification of model and save path

--- SIDM_models/convolution_based/SIDM_convolution_mapper.py
+++ SIDM_models/convolution_based/SIDM_convolution_mapper.py
@@ -6,61 +6,77 @@ import climate_learn as cl
 import numpy as np
 import os
 
-from climate_learn.models.hub.sidm import dH_to_dT_conv
+from climate_learn.models.hub.sidm import dH_to_dT_conv, dH_to_dT_conv_PE
 from climate_learn.metrics.metrics import DISA_abs_horizontal_vertical_differences
-
-# ---------------------- DATASET ----------------------
-dm = cl.data.IterDataModule(
-    "direct-forecasting",
-    "dataset/CERRA-534/",
-    "dataset/CERRA-534/",
-    ["2m_temperature"],
-    ["2m_temperature"],
-    src="era5",
-    history=1,
-    window=1,
-    pred_range=1,
-    batch_size=1,
-    num_workers=4,
-)
-dm.setup()
-train_loader = dm.train_dataloader()
-
-
-# ---------------------- STATIC TERRAIN ----------------------
-oro_path = "dataset/CERRA-534/orography.npz"
-oro = np.load(oro_path)["orography"].astype(np.float32)
-H = torch.tensor(oro) # Ensure shape is (1, 534, 534)
-dH_stack = DISA_abs_horizontal_vertical_differences(H, output="concat",soft=False) # (B,2, H, W)
-
-# ---------------------- MODEL ----------------------
-model = dH_to_dT_conv().cuda()
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
-loss_fn = nn.MSELoss()
+def sidm_convolution_mapper_training(_model, save_path):
+    # ---------------------- DATASET ----------------------
+    dm = cl.data.IterDataModule(
+        "direct-forecasting",
+        "dataset/CERRA-534/",
+        "dataset/CERRA-534/",
+        ["2m_temperature"],
+        ["2m_temperature"],
+        src="era5",
+        history=1,
+        window=1,
+        pred_range=1,
+        batch_size=1,
+        num_workers=4,
+    )
+    dm.setup()
+    train_loader = dm.train_dataloader()
 
 
-# ---------------------- TRAINING ----------------------
-for batch in tqdm(train_loader, total=8640):
-    T, y, _, _ = batch
-    T = T.squeeze(1,2).float().cuda()  # (H, W)
-    
-    with torch.no_grad(): # Compute target dT (NO grad tracking)
-        dT_stack = DISA_abs_horizontal_vertical_differences(T, output="concat",soft=False) # (B,2, H, W)
+    # ---------------------- STATIC TERRAIN ----------------------
+    oro_path = "dataset/CERRA-534/orography.npz"
+    oro = np.load(oro_path)["orography"].astype(np.float32)
+    H = torch.tensor(oro) # Ensure shape is (1, 534, 534)
+    dH_stack = DISA_abs_horizontal_vertical_differences(H, output="concat",soft=False) # (B,2, H, W)
 
-    # Forward & Loss
-    dT_stack_pred = model(dH_stack)
-    loss = loss_fn(dT_stack_pred, dT_stack)
-
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-print("Training complete.")
+    # ---------------------- MODEL ----------------------
+    model = _model.cuda()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+    loss_fn = nn.MSELoss()
 
 
-# ---------------------- SAVE --------------------
-save_path = "outputs/SIDM_models/SIDM_convolution_based/dH_to_dT_conv.pt"
-save_dir = os.path.dirname(save_path)
+    # ---------------------- TRAINING ----------------------
+    for batch in tqdm(train_loader, total=8640):
+        T, y, _, _ = batch
+        T = T.squeeze(1,2).float().cuda()  # (H, W)
+        
+        with torch.no_grad(): # Compute target dT (NO grad tracking)
+            dT_stack = DISA_abs_horizontal_vertical_differences(T, output="concat",soft=False) # (B,2, H, W)
 
-os.makedirs(save_dir, exist_ok=True) # Create directory if it doesn't exist
-torch.save(model.state_dict(), save_path)
-print("Model saved to", save_path)
+        # Forward & Loss
+        dT_stack_pred = model(dH_stack)
+        loss = loss_fn(dT_stack_pred, dT_stack)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    print("Training complete.")
+
+
+    # ---------------------- SAVE --------------------
+    save_path = "outputs/SIDM_models/SIDM_convolution_based/dH_to_dT_conv.pt"
+    save_dir = os.path.dirname(save_path)
+
+    os.makedirs(save_dir, exist_ok=True) # Create directory if it doesn't exist
+    torch.save(model.state_dict(), save_path)
+    print("Model saved to", save_path)
+
+if __name__ == "__main__":
+    #model = dH_to_dT_conv()
+    #save_path = "outputs/SIDM_models/SIDM_convolution_based/dH_to_dT_conv.pt"
+    model = dH_to_dT_conv_PE(
+        img_size=(534,534),
+        in_channels=2,
+        out_channels=2,
+        n_sh_coeff=36,
+        conv_start_size=32,
+        conv_start_size_enc=64,
+        siren_hidden=128,
+        oro_path="dataset/CERRA-534/orography.npz"
+    )
+    save_path = "outputs/SIDM_models/SIDM_convolution_based/dH_to_dT_conv_PE.pt"
+    sidm_convolution_mapper_training(model, save_path=save_path)
